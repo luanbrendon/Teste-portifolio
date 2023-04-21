deaae86135cf8fbf04f0043bd470532c2414cdf9
LUAN BRENDON <luan_brendonmacedo@hotmail.com>
2023-04-21 14:52:37 -0300

    finalizando algumas mudancas e adicionando footer e meus projetos

--- script.js
+++ script.js
@@ -64,3 +64,86 @@ function sobreMim() {
 }
 
 sobreMim();
+
+
+const listaALL = document.querySelectorAll('.projetos_armazenados ul li');
+const buttonGeral = document.querySelectorAll('.project_models ul li');
+const buttonAll = document.querySelector('.project_models .all');
+
+listaALL.forEach((item)=>{
+  item.classList.add('ativo');
+})
+
+function removeClick(index){
+  buttonGeral.forEach((item)=>{
+    item.classList.remove('ativo');
+  })
+  buttonGeral[index].classList.add('ativo');
+}
+
+buttonGeral.forEach((event,index)=>{
+  event.addEventListener('click', ()=>{
+    removeClick(index)
+  })
+})
+
+
+function showLista(lista, buttom = "all") {
+  lista.forEach((item)=>{
+    item.classList.remove('ativo');
+  })
+
+  if(buttom == 'design') {
+    lista[0].classList.add('ativo');
+    lista[1].classList.add('ativo');
+    lista[2].classList.add('ativo');
+  }
+
+  if(buttom == 'graphic') {
+    
+    lista[3].classList.add('ativo');
+    lista[4].classList.add('ativo');
+    
+  }
+
+  if(buttom == 'websites') {
+    lista[5].classList.add('ativo');
+    lista[6].classList.add('ativo');
+    lista[7].classList.add('ativo');
+  }
+
+  if(buttom == 'all') {
+    lista[0].classList.add('ativo');
+    lista[1].classList.add('ativo');
+    lista[2].classList.add('ativo');
+    lista[3].classList.add('ativo');
+    lista[4].classList.add('ativo');
+    lista[5].classList.add('ativo');
+    lista[6].classList.add('ativo');
+    lista[7].classList.add('ativo');
+  }
+
+}
+
+
+buttonGeral.forEach((item)=>{
+  item.addEventListener('click', (e)=>{
+    let currentButton = e.target;
+    if(currentButton.classList.contains('all')){
+      showLista(listaALL);
+    }
+    if(currentButton.classList.contains('design')){
+      showLista(listaALL, "design");
+    }
+    if(currentButton.classList.contains('graphic')){
+      showLista(listaALL,"graphic");
+    }
+    if(currentButton.classList.contains('websites')){
+      showLista(listaALL, "websites");
+    }
+    if(currentButton.classList.contains('all')){
+      showLista(listaALL,"all");
+    }
+  })
+})
+
