67682422c7b0bc29899ca15d3e6542dea73056d6
LUAN BRENDON <luan_brendonmacedo@hotmail.com>
2024-05-24 19:35:00 -0300

    criando um efeito enquando estou na viweport do sobre mim

--- script.js
+++ script.js
@@ -156,3 +156,27 @@ buttonGeral.forEach((item)=>{
   })
 })
 
+
+
+/*efeito aparecer */
+document.addEventListener('DOMContentLoaded', function () {
+  const observerOptions = {
+    root: null,
+    rootMargin: '0px',
+    threshold: 0.2 
+  };
+
+  const observer = new IntersectionObserver((entries, observer) => {
+    entries.forEach(entry => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add('aparecer');
+        observer.unobserve(entry.target);
+      }
+    });
+  }, observerOptions);
+
+  const targets = document.querySelectorAll('.sobre img, .sobre_conteudo');
+  targets.forEach (target => {
+    observer.observe(target);
+  });
+});
